c9d4ecc03219fa31b76615fd15f760d2e13a5198
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-12-20 14:29:32 +0300

    DTO CommandResult

---
+++ src/main/java/artopia/services/command/CommandResult.java
@@ -0,0 +1,12 @@
+package artopia.services.command;
+
+/**
+ * @author Rottenwood
+ */
+public class CommandResult {
+    String commandName;
+
+    public CommandResult(String command) {
+        this.commandName = command;
+    }
+}
