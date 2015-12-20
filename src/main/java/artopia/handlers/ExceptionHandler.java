c00e13f0d345fc1c42cb5bcbddd50cf31a5d976c
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-12-21 00:31:19 +0300

    Класс обработки эксепшенов

---
+++ src/main/java/artopia/handlers/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package artopia.handlers;
+
+/**
+ * @author Rottenwood
+ */
+public class ExceptionHandler {
+    public static void handle(Exception exception) {
+        exception.printStackTrace();
+    }
+}
