7e18fe9b13ade4868980cf49695317a12bc63e24
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-12-25 23:39:41 +0300

    Тест для ArrayHelper.join()

--- src/main/java/vehsamrak/helpers/ArrayHelper.java
+++ src/main/java/vehsamrak/helpers/ArrayHelper.java
@@ -10,12 +10,19 @@ public class ArrayHelper {
     /**
      * Преобразование ArrayList в строку
      * @param arrayList ArrayList
+     * @param delimiter Разделитель
      * @return String
      */
-    public static String join(ArrayList<String> arrayList) {
+    public static String join(ArrayList<String> arrayList, String delimiter) {
         StringBuilder stringBuilder = new StringBuilder();
+
+        int i = 1;
         for (String s : arrayList) {
             stringBuilder.append(s);
+
+            if (i++ != arrayList.size()) {
+                stringBuilder.append(delimiter);
+            }
         }
 
         return stringBuilder.toString();
