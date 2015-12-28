70bf76d23b9d17c27d931ce8d5df30bbedc95404
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-12-28 23:55:29 +0300

    Сервис создания сессии для БД измене: создание фабрики сессий происходит только один раз

--- src/main/java/artopia/services/DatabaseService.java
+++ src/main/java/artopia/services/DatabaseService.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Vehsamrak
@@ -14,22 +13,27 @@ import org.jetbrains.annotations.Nullable;
 public class DatabaseService
 {
     private static Session session = null;
+    private static SessionFactory sessionFactory = null;
 
-    @Nullable
-    private static Session openSession() {
+    private static SessionFactory createSessionFactory() {
         // A SessionFactory is set up once for an application!
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         try {
-            SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-
-            return sessionFactory.openSession();
+            return new MetadataSources(registry).buildMetadata().buildSessionFactory();
         } catch (Exception e) {
             // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
             // so destroy it manually.
             StandardServiceRegistryBuilder.destroy(registry);
+            throw e;
+        }
+    }
+
+    private static Session openSession() {
+        if (sessionFactory == null) {
+            sessionFactory = createSessionFactory();
         }
 
-        return null;
+        return sessionFactory.openSession();
     }
 
     public static void close() {
