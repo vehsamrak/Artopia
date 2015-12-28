c41a7fc6512e03f6a28b992d13eccaf999eddd14
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-12-29 00:03:29 +0300

    Поиск по БД юзеров при логине, запись новых юзеров в БД

--- src/main/java/artopia/services/UserService.java
+++ src/main/java/artopia/services/UserService.java
@@ -3,6 +3,9 @@ package artopia.services;
 import artopia.exceptions.EmptyPassword;
 import artopia.exceptions.EmptyUsername;
 import artopia.models.User;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * @author Rottenwood
@@ -10,8 +13,20 @@ import artopia.models.User;
 public class UserService
 {
     public static User login(String username, String password) throws EmptyPassword, EmptyUsername {
-        User user = new User(username, password);
-        user.authenticate();
+        Session session = DatabaseService.getSession();
+
+        Query query = session.createQuery("FROM User WHERE username=:username");
+        query.setParameter("username", username);
+        User user = (User) query.uniqueResult();
+
+        if (user == null) {
+            user = new User(username, password);
+            user.authenticate();
+
+            Transaction transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        }
 
         System.out.printf("[+] %s зашел в игру.%n", user.getUsername());
 
