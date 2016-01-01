df49eb230fa2d85674f379ce693a3923ac8e5c4b
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-02 01:10:41 +0300

    NullOutputStream для использования в тестах: System.setOut(new PrintStream(new NullOutputStream()));

---
+++ src/main/java/vehsamrak/helpers/NullOutputStream.java
@@ -0,0 +1,13 @@
+package vehsamrak.helpers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Vehsamrak
+ */
+public class NullOutputStream extends OutputStream
+{
+    @Override
+    public void write(int b) throws IOException {}
+}
