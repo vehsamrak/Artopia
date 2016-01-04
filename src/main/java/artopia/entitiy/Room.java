ee95e2f54eee1c7ba2c4d5cb5872603f975c1aa4
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-04 14:55:10 +0300

    В сущность Room добавлены связи с окружающими комнатами

--- src/main/java/artopia/entitiy/Room.java
+++ src/main/java/artopia/entitiy/Room.java
@@ -5,9 +5,80 @@ package artopia.entitiy;
  */
 public class Room
 {
-    private long id;
-
+    private String id;
     private String name;
-
     private String desciption;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
+    private Room up;
+    private Room down;
+
+    public Room(String id, String name, String desciption)
+    {
+        this.id = id;
+        this.name = name;
+        this.desciption = desciption;
+    }
+
+    public Room getNorth()
+    {
+        return north;
+    }
+
+    public void setNorth(Room room)
+    {
+        this.north = room;
+    }
+
+    public Room getSouth()
+    {
+        return south;
+    }
+
+    public void setSouth(Room room)
+    {
+        this.south = room;
+    }
+
+    public Room getEast()
+    {
+        return east;
+    }
+
+    public void setEast(Room room)
+    {
+        this.east = room;
+    }
+
+    public Room getWest()
+    {
+        return west;
+    }
+
+    public void setWest(Room room)
+    {
+        this.west = room;
+    }
+
+    public Room getUp()
+    {
+        return up;
+    }
+
+    public void setUp(Room room)
+    {
+        this.up = room;
+    }
+
+    public Room getDown()
+    {
+        return down;
+    }
+
+    public void setDown(Room room)
+    {
+        this.down = room;
+    }
 }
