55d6c17680d7bfaec8ba4bc6d7154bfc352e77ce
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-10 02:37:09 +0300

    Тест для RoomParser

---
+++ src/test/java/artopia/service/room/RoomParserTest.java
@@ -0,0 +1,25 @@
+package artopia.service.room;
+
+import artopia.entitiy.Room;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * @author Vehsamrak
+ */
+public class RoomParserTest extends Assert
+{
+
+    @Test
+    public void getRooms() throws Exception
+    {
+        RoomParser roomParser = new RoomParser();
+        File zoneFile = new File("src/test/resources/zones/test.xml");
+        ArrayList<Room> roomList = roomParser.parseRooms(zoneFile);
+
+        assertEquals(2, roomList.size());
+    }
+}
