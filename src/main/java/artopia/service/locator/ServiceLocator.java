4289e4726f9ea2518df17550f4c62286108f53e6
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-10 03:12:03 +0300

    Внедрение парсера в RoomRepository в сервис локаторе. Репозиторий теперь использует парсер для получения данных зон

--- src/main/java/artopia/service/locator/ServiceLocator.java
+++ src/main/java/artopia/service/locator/ServiceLocator.java
@@ -4,6 +4,7 @@ import artopia.exception.ServiceNotFound;
 import artopia.service.DatabaseService;
 import artopia.service.UserService;
 import artopia.service.command.CommandService;
+import artopia.service.room.RoomParser;
 import artopia.service.room.RoomRepository;
 
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ public class ServiceLocator
         this.serviceList.add(databaseService);
         this.serviceList.add(new CommandService(this));
         this.serviceList.add(new UserService(databaseService));
-        this.serviceList.add(new RoomRepository());
+        this.serviceList.add(new RoomRepository(new RoomParser()));
     }
 
     public AbstractService get(Service serviceName) throws ServiceNotFound
--- src/main/java/artopia/service/room/RoomRepository.java
+++ src/main/java/artopia/service/room/RoomRepository.java
@@ -4,6 +4,7 @@ import artopia.entitiy.Room;
 import artopia.service.locator.AbstractService;
 import artopia.service.locator.Service;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -11,73 +12,15 @@ import java.util.ArrayList;
  */
 public class RoomRepository extends AbstractService
 {
-    final private ArrayList<Room> roomList = new ArrayList<>();
+    private ArrayList<Room> roomList = new ArrayList<>();
 
     /**
      * Список всех комнат
      * TODO: 04.01.16 Должно быть вынесено в отдельный конфиг
      */
-    public RoomRepository()
+    public RoomRepository(RoomParser roomParser)
     {
-        this.roomList.add(new Room(
-                "system-start",
-                "Центр Мира",
-                "Ты находишься в самом центре мира.",
-                "system-north",
-                "system-east",
-                "system-south",
-                "system-west",
-                "system-up",
-                "system-down"
-        ));
-
-        this.roomList.add(new Room(
-                "system-north",
-                "Северный Полюс",
-                "Холодные пустоши смертоносного льда.",
-                null,
-                null,
-                "system-start",
-                null,
-                null,
-                null
-        ));
-
-        this.roomList.add(new Room(
-                "system-east",
-                "Восточный Край",
-                "Жаркие пустыни востока.",
-                null,
-                null,
-                null,
-                "system-start",
-                null,
-                null
-        ));
-
-        this.roomList.add(new Room(
-                "system-south",
-                "Южный Полюс",
-                "Снежный буран приближается со всех сторон.",
-                "system-start",
-                null,
-                null,
-                null,
-                null,
-                null
-        ));
-
-        this.roomList.add(new Room(
-                "system-west",
-                "Западный Край",
-                "Ветренные прерии западного края.",
-                null,
-                "system-start",
-                null,
-                null,
-                null,
-                null
-        ));
+        this.roomList = roomParser.parseRooms(new File("src/main/resources/zones/medieval-town.xml"));
     }
 
     /**
