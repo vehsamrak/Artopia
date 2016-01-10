f9a787fdafd270264eaa29d5980cc5a5efbf5187
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-10 03:07:40 +0300

    Рефакторинг теста RoomRepositoryTest

--- src/test/java/artopia/service/room/RoomRepositoryTest.java
+++ src/test/java/artopia/service/room/RoomRepositoryTest.java
@@ -13,7 +13,7 @@ public class RoomRepositoryTest extends Assert
     @Test
     public void findById_repositoryWithoutAskedRoomId_returnsNull() throws Exception
     {
-        RoomRepository roomRepository = new RoomRepository();
+        RoomRepository roomRepository = this.createRoomRepository();
         Room unexistingRoom = roomRepository.findById("test-test-test-test");
 
         assertNull(unexistingRoom);
@@ -22,9 +22,15 @@ public class RoomRepositoryTest extends Assert
     @Test
     public void findById_repositoryWithAskedRoomId_returnsRoom()
     {
-        RoomRepository roomRepository = new RoomRepository();
+        RoomRepository roomRepository = this.createRoomRepository();
         Room testRoom1 = roomRepository.findById("system-start");
 
         assertNotNull(testRoom1);
+    }
+
+    private RoomRepository createRoomRepository()
+    {
+        RoomParser roomParser = new RoomParser();
+        return new RoomRepository(roomParser);
     }
 }
