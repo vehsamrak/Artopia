71e03370b79aeb7c7e0f7c58b3cf5f73d9cf4930
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-10 14:35:18 +0300

    Сущность Exit представляет выход из комнаты

---
+++ src/main/java/artopia/entitiy/room/Exit.java
@@ -0,0 +1,39 @@
+package artopia.entitiy.room;
+
+/**
+ * Выход из комнаты
+ * @author Vehsamrak
+ */
+public class Exit
+{
+    private String roomId;
+    private String doorName;
+    private boolean closed = true;
+
+    public Exit(String roomId)
+    {
+        this.roomId = roomId;
+    }
+
+    public Exit(String roomId, String doorName, boolean closed)
+    {
+        this.roomId = roomId;
+        this.doorName = doorName;
+        this.closed = closed;
+    }
+
+    public String getRoomId()
+    {
+        return roomId;
+    }
+
+    public String getDoorName()
+    {
+        return doorName;
+    }
+
+    public boolean isClosed()
+    {
+        return closed;
+    }
+}
