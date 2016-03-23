68d90e16a471cb6b576c83952ec29470ab95837e
wooferclaw <grigoryfrants@gmail.com>
2016-03-23 19:05:33 +0300

    Test getErrorMessage method update. Style fixed.

--- src/test/java/artopia/service/command/errors/RoomClosedTest.java
+++ src/test/java/artopia/service/command/errors/RoomClosedTest.java
@@ -1,31 +1,24 @@
 package artopia.service.command.errors;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 /**
  * Created by wooferclaw on 23.03.2016.
  */
-public class RoomClosedTest {
-
-    private String doorMessage;
+public class RoomClosedTest extends Assert{
 
     @Test
-    public void testGetErrorMessageDoorClosed() throws Exception {
-        if (doorMessage == null) {
-            doorMessage = "Дверь закрыта";
-            RoomClosed roomClosed = new RoomClosed(doorMessage);
-            assertEquals(roomClosed.getErrorMessage(), "Ты не можешь пройти туда. " + this.doorMessage + ".");
-        }
+    public void testGetErrorMessage_DoorClosed_Null() throws Exception {
+            RoomClosed roomClosed = new RoomClosed(null);
+            assertEquals(roomClosed.getErrorMessage(), "Ты не можешь пройти туда. Дверь закрыта.");
     }
 
+
     @Test
-    public void testGetErrorMessageCharacter() throws Exception {
-        if (doorMessage != null) {
-            doorMessage = Character.toString(doorMessage.charAt(0)).toUpperCase() + doorMessage.substring(1);
-            RoomClosed roomClosed = new RoomClosed(doorMessage);
-            assertEquals(roomClosed.getErrorMessage(), "Ты не можешь пройти туда. " + this.doorMessage + ".");
-        }
+    public void testGetErrorMessage_DoorClosed_String() throws Exception {
+            RoomClosed roomClosed = new RoomClosed("Тестовая строка");
+            assertEquals(roomClosed.getErrorMessage(), "Ты не можешь пройти туда. Тестовая строка.");
     }
 }
