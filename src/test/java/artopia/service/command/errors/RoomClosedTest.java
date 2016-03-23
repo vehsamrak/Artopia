02b324a250d6b5773627203795736f9bde124224
wooferclaw <grigoryfrants@gmail.com>
2016-03-23 20:10:41 +0300

    Test getErrorMessage method update 2. Style fixed, methods renamed

--- src/test/java/artopia/service/command/errors/RoomClosedTest.java
+++ src/test/java/artopia/service/command/errors/RoomClosedTest.java
@@ -10,15 +10,17 @@ import org.junit.Test;
 public class RoomClosedTest extends Assert{
 
     @Test
-    public void testGetErrorMessage_DoorClosed_Null() throws Exception {
+    public void getErrorMessage_nullConstructor_errorMessageWithClosedDoorPhrase() throws Exception {
             RoomClosed roomClosed = new RoomClosed(null);
+
             assertEquals(roomClosed.getErrorMessage(), "Ты не можешь пройти туда. Дверь закрыта.");
     }
 
 
     @Test
-    public void testGetErrorMessage_DoorClosed_String() throws Exception {
+    public void getErrorMessage_nullConstructor_errorMessageWithOtherPhrase() throws Exception {
             RoomClosed roomClosed = new RoomClosed("Тестовая строка");
+
             assertEquals(roomClosed.getErrorMessage(), "Ты не можешь пройти туда. Тестовая строка.");
     }
 }
