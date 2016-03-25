0f8a40ea862cb5eb0fa89929e035713c2191d184
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-03-25 22:57:28 +0300

    Вся логика вынесена из класса Main в Launcher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,10 @@
-import artopia.handler.ConnectionHandler;
-import artopia.handler.ExceptionHandler;
-import artopia.service.locator.ServiceLocator;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import artopia.Launcher;
 
 /**
  * @author Rottenwood
  */
 public class Main {
     public static void main(String[] args) {
-        int port = 9000;
-
-        System.out.println("Инициализация приложения ...");
-        ServiceLocator serviceLocator = new ServiceLocator();
-
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-
-            System.out.printf("Сервер запущен на порту %s ...%n", port);
-
-            while (true) {
-                Socket socket = serverSocket.accept();
-
-                BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter socketOutput = new PrintWriter(socket.getOutputStream(), true);
-
-                Runnable connectionHandler = new ConnectionHandler(
-                        socket,
-                        socketInput,
-                        socketOutput,
-                        serviceLocator
-                );
-
-                new Thread(connectionHandler).start();
-
-            }
-        } catch (Exception exception) {
-            ExceptionHandler.handle(exception);
-        }
+        Launcher.run();
     }
 }
---
+++ src/main/java/artopia/Launcher.java
@@ -0,0 +1,49 @@
+package artopia;
+
+import artopia.handler.ConnectionHandler;
+import artopia.handler.ExceptionHandler;
+import artopia.service.locator.ServiceLocator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author Vehsamrak
+ */
+public class Launcher
+{
+    public static void run()
+    {
+        int port = 9000;
+
+        System.out.println("Инициализация приложения ...");
+        ServiceLocator serviceLocator = new ServiceLocator();
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            System.out.printf("Сервер запущен на порту %s ...%n", port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter socketOutput = new PrintWriter(socket.getOutputStream(), true);
+
+                Runnable connectionHandler = new ConnectionHandler(
+                        socket,
+                        socketInput,
+                        socketOutput,
+                        serviceLocator
+                );
+
+                new Thread(connectionHandler).start();
+            }
+        } catch (Exception exception) {
+            ExceptionHandler.handle(exception);
+        }
+    }
+}
